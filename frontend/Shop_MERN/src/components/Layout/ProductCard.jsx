b49835677ebb9ecc604e9f08607da5f4cf12da36
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-08-19 22:04:21 +0600

    Product details added

--- frontend/Shop_MERN/src/components/Layout/ProductCard.jsx
+++ frontend/Shop_MERN/src/components/Layout/ProductCard.jsx
@@ -1,17 +1,23 @@
-// import React from 'react';
-
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'; // Import PropTypes
 import styles from '../../styles/styles';
-import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import {
+  AiFillHeart,
+  AiFillStar,
+  AiOutlineEye,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+  AiOutlineStar,
+} from 'react-icons/ai';
+import ProductDetailsCard from './ProductDetailsCard';
 
 const ProductCard = ({ data }) => {
   const [click, setClick] = useState(false);
   const [open, setOpen] = useState(false);
 
-  const d = data.name;
-  // eslint-disable-next-line react/prop-types
-  const product_name = d.replace(/\s+/g, '-');
+  const { name, image_Url, shop, price, discount_price, total_sell } = data;
+  const product_name = name.replace(/\s+/g, '-');
 
   return (
     <div>
@@ -19,19 +25,18 @@ const ProductCard = ({ data }) => {
         <div className="flex justify-end"></div>
         <Link to={`/product/${product_name}`}>
           <img
-            src={`${data.image_Url[0].url}`}
+            src={`${image_Url[0].url}`}
             alt="Product Image"
             className="w-full h-[170px] object-contain"
           />
         </Link>
 
-        {/* side options */}
         <Link to={'/'}>
-          <h5 className={`${styles.shop_name}`}>{data.shop.name}</h5>
+          <h5 className={`${styles.shop_name}`}>{shop.name}</h5>
         </Link>
         <Link to={`/product/${product_name}`}>
           <h4 className="pb-3 font-[500]">
-            {data.name.length > 40 ? data.name.slice(0, 40) + '...' : data.name}
+            {name.length > 40 ? name.slice(0, 40) + '...' : name}
           </h4>
 
           <div className="flex text-yellow-400">
@@ -48,20 +53,75 @@ const ProductCard = ({ data }) => {
           <div className="py-2 flex items-center justify-between">
             <div className="flex">
               <h5 className={`${styles.productDiscountPrice}`}>
-                {data.price === 0 ? data.price : data.discount_price}$
+                {price === 0 ? price : discount_price}$
               </h5>
               <h4 className={`${styles.price}`}>
-                {data.price ? data.price + ' $' : null}
+                {price ? price + ' $' : null}
               </h4>
             </div>
             <span className="font-[400] text-[17px] text-[#68d284]">
-              {data?.total_sell} sold
+              {total_sell} sold
             </span>
           </div>
         </Link>
+
+        {/* side options */}
+        <div>
+          {click ? (
+            <AiFillHeart
+              size={22}
+              className="cursor-pointer absolute right-2 top-5"
+              onClick={() => setClick(!click)}
+              color={click ? 'red' : '#333'}
+              title="Remove from wishlist"
+            />
+          ) : (
+            <AiOutlineHeart
+              size={22}
+              className="cursor-pointer absolute right-2 top-5"
+              onClick={() => setClick(!click)}
+              color={click ? 'red' : '#333'}
+              title="Add to wishlist"
+            />
+          )}
+          <AiOutlineEye
+            size={22}
+            className="cursor-pointer absolute right-2 top-14"
+            onClick={() => setOpen(!open)}
+            color="#333"
+            title="Quick view"
+          />
+          <AiOutlineShoppingCart
+            size={25}
+            className="cursor-pointer absolute right-2 top-24"
+            color="#444"
+            onClick={() => setOpen(!open)}
+            title="Add to cart"
+          />
+          {open ? <ProductDetailsCard setOpen={setOpen} data={data} /> : null}
+        </div>
       </div>
     </div>
   );
+};
+
+// Define PropTypes for the 'data' prop
+ProductCard.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image_Url: PropTypes.arrayOf(
+      PropTypes.shape({
+        url: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    shop: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    price: PropTypes.number,
+    discount_price: PropTypes.number,
+    total_sell: PropTypes.number,
+    // Add more prop types as needed
+  }).isRequired,
 };
 
 export default ProductCard;
--- frontend/Shop_MERN/src/routs.js
+++ frontend/Shop_MERN/src/routs.js
@@ -5,5 +5,15 @@ import Hero from './components/Layout/Hero.jsx';
 import Header from './components/Layout/Header.jsx';
 import Categories from './components/Layout/Categories.jsx';
 import BestDeals from './components/Layout/BestDeals.jsx';
+import ProductDetailsCard from './components/Layout/ProductDetailsCard.jsx';
 
-export { Login, SignUp, Activation, Hero, Header, Categories, BestDeals };
+export {
+  Login,
+  SignUp,
+  Activation,
+  Hero,
+  Header,
+  Categories,
+  BestDeals,
+  ProductDetailsCard,
+};
